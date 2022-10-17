d417ce2da603aef80d8ea573f8a483429bf6db53
Kilmurray <66964622+MaddyKilmurray@users.noreply.github.com>
2022-10-17 11:45:15 +0100

    First commit

---
+++ src/main/java/com/sparta/thefightingsheep/TheFightingSheepApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.thefightingsheep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFightingSheepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheFightingSheepApplication.class, args);
+    }
+
+}
