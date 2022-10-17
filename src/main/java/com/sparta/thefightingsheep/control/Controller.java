bc89e1dbc7376bc7355f41bb64a63b9853d4ad67
Abdullah Navid <abdullahnavid5@gmail.com>
2022-10-17 15:47:02 +0100

    added the @ keywords to the controller classes

--- src/main/java/com/sparta/thefightingsheep/control/Controller.java
+++
@@ -1,4 +0,0 @@
-package com.sparta.thefightingsheep.control;
-
-public class Controller {
-}
---
+++ src/main/java/com/sparta/thefightingsheep/control/WebController.java
@@ -0,0 +1,7 @@
+package com.sparta.thefightingsheep.control;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class WebController {
+}
