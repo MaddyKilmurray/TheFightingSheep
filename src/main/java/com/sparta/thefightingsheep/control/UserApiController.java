2c5b428ce446737de6befd40c6cc1a0ef6339c40
Kilmurray <66964622+MaddyKilmurray@users.noreply.github.com>
2022-10-18 11:46:49 +0100

    Security commits

--- src/main/java/com/sparta/thefightingsheep/control/UserApiController.java
+++ src/main/java/com/sparta/thefightingsheep/control/UserApiController.java
@@ -3,6 +3,7 @@ package com.sparta.thefightingsheep.control;
 import com.sparta.thefightingsheep.model.user.User;
 import com.sparta.thefightingsheep.model.user.UserDAO;
 import com.sparta.thefightingsheep.model.user.UserRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,8 +19,8 @@ public class UserApiController {
     private UserDAO userDAO;
 
     @GetMapping("/user/{id}")
-    public User getUserById(@PathVariable int id){
-        User result = userRepo.findById(String.valueOf(id)).get();
+    public User getUserById(@PathVariable String id){
+        User result = userRepo.findById(new ObjectId(id)).get();
         return result;
     }
 }
--- src/main/java/com/sparta/thefightingsheep/model/user/UserRepository.java
+++ src/main/java/com/sparta/thefightingsheep/model/user/UserRepository.java
@@ -1,12 +1,15 @@
 package com.sparta.thefightingsheep.model.user;
 
+import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.Optional;
 
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<User, ObjectId> {
     
     @Query("{name:'?0'}")
     Optional<User> findUserByName(String name);
+
+    Optional<User> findById(ObjectId id);
 }
