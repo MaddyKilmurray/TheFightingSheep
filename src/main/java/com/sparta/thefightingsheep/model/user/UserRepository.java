b3041e1c2da09faaea903169842d0c65cf2630f8
Malek <malekbusari@gmail.com>
2022-10-18 11:45:02 +0100

    Add UserEntity + DTO + DAO

--- src/main/java/com/sparta/thefightingsheep/model/user/UserRepository.java
+++ src/main/java/com/sparta/thefightingsheep/model/user/UserRepository.java
@@ -1,10 +1,11 @@
 package com.sparta.thefightingsheep.model.user;
 
+import com.sparta.thefightingsheep.model.entity.UsersEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
 
-public interface UserRepository extends MongoRepository<User, String> {
+public interface UserRepository extends MongoRepository<UsersEntity, String> {
 
     Optional<User> findUserByName(String name);
 }
