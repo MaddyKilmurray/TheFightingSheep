434f7060f7a6beeedafdba3be8a24666234213f5
Andrei Cravtov <the.andrei.cravtov@gmail.com>
2022-10-19 14:42:34 +0100

    commit

--- src/main/java/com/sparta/thefightingsheep/modelm/entity/user/AuthorisedUser.java
+++ src/main/java/com/sparta/thefightingsheep/modelm/entity/user/AuthorisedUser.java
@@ -1,7 +1,9 @@
 package com.sparta.thefightingsheep.modelm.entity.user;
 
+import com.sparta.thefightingsheep.modelm.entity.role.Role;
 import lombok.Data;
 import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.MongoId;
@@ -13,18 +15,18 @@ public class AuthorisedUser {
     private ObjectId id;
     private String username;
     private String password;
-    @Field("userRoles")
-    private UserRole userRole;
+    @DBRef
+    private Role userRole;
 
     public AuthorisedUser() {}
 
-    public AuthorisedUser(String username, String password, UserRole userRole) {
+    public AuthorisedUser(String username, String password, Role userRole) {
         this.username = username;
         this.password = password;
         this.userRole = userRole;
     }
 
-    public AuthorisedUser(ObjectId id, String username, String password, UserRole userRole) {
+    public AuthorisedUser(ObjectId id, String username, String password, Role userRole) {
         this.id = id;
         this.username = username;
         this.password = password;
