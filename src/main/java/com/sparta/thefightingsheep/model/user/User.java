2b3cac2b75d57c24ae2ae7e2fc4f0161d0905931
Malek <malekbusari@gmail.com>
2022-10-19 09:01:35 +0100

    Connected User class to users in database

--- src/main/java/com/sparta/thefightingsheep/model/user/User.java
+++ src/main/java/com/sparta/thefightingsheep/model/user/User.java
@@ -1,61 +1,17 @@
 package com.sparta.thefightingsheep.model.user;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
+@Data
+@Document(collection = "users")
 public class User {
-	private String password;
+	@Id
+	private String id;
 	private String name;
-	private @MongoId ObjectId id;
 	private String email;
-
-	public User(String password, String name, ObjectId id, String email) {
-		this.password = password;
-		this.name = name;
-		this.id = id;
-		this.email = email;
-	}
-
-	public void setPassword(String password){
-		this.password = password;
-	}
-
-	public String getPassword(){
-		return password;
-	}
-
-	public void setName(String name){
-		this.name = name;
-	}
-
-	public String getName(){
-		return name;
-	}
-
-	public void setId(String id){
-		this.id = new ObjectId(id);
-	}
-
-	public ObjectId getId(){
-		return id;
-	}
-
-	public void setEmail(String email){
-		this.email = email;
-	}
-
-	public String getEmail(){
-		return email;
-	}
-
-	@Override
- 	public String toString(){
-		return 
-			"Response{" + 
-			"password = '" + password + '\'' + 
-			",name = '" + name + '\'' + 
-			",_id = '" + id + '\'' + 
-			",email = '" + email + '\'' + 
-			"}";
-		}
+	private String password;
 }
