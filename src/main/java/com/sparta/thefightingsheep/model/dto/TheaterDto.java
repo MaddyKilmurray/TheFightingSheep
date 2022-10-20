20845a8ac2570a3845b49d636d617c84df357c35
Malek <malekbusari@gmail.com>
2022-10-20 11:02:38 +0100

    Add TheaterDao and TheaterDto

--- src/main/java/com/sparta/thefightingsheep/model/dto/TheaterDto.java
+++ src/main/java/com/sparta/thefightingsheep/model/dto/TheaterDto.java
@@ -1,4 +1,26 @@
 package com.sparta.thefightingsheep.model.dto;
 
-public class TheaterDto {
+
+import com.sparta.thefightingsheep.model.entity.theater.Location;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = false)
+public class TheaterDto extends Dto {
+    private Integer theaterId;
+    private Location location;
+
+    public TheaterDto() {}
+
+    public TheaterDto(Integer theaterId, Location location) {
+        this.theaterId = theaterId;
+        this.location = location;
+    }
+
+    public TheaterDto(String id, Integer theaterId, Location location) {
+        super(id);
+        this.theaterId = theaterId;
+        this.location = location;
+    }
 }
