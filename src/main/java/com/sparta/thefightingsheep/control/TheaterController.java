4cfa57141da5874c41e94da355af4cbcda33c54a
Abdullah Navid <abdullahnavid5@gmail.com>
2022-10-20 14:08:55 +0100

    fixed some errors and created new errors

--- src/main/java/com/sparta/thefightingsheep/control/TheaterController.java
+++ src/main/java/com/sparta/thefightingsheep/control/TheaterController.java
@@ -2,8 +2,10 @@ package com.sparta.thefightingsheep.control;
 
 import com.sparta.thefightingsheep.model.entity.theater.Theater;
 import com.sparta.thefightingsheep.model.repository.TheaterRepository;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,8 +21,14 @@ public class TheaterController {
 //    @Autowired
 //    private TheaterDao dao;
 
-    @GetMapping("/all")
+    @GetMapping("/find/all")
     public List<Theater> getAllTheaters() {
         return theaterRepo.findAll();
+    }
+
+    @GetMapping("/find/{id}")
+    public Theater getTheaterById(@PathVariable String id) {
+        Theater theater = theaterRepo.findById(new ObjectId(id).get()).get();
+        return theater;
     }
 }
